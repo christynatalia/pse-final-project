2e087faa6b45dfb2682eb3b97aaf702406c657db
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-24 09:05:50 +0700

    navbar dropdown not working
    Added the navbar dropdown for notifications but it's not working properly.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,32 @@
 import React, {useState} from 'react';
-import  { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
-            <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-                <span>
-                    PSE Project
-                </span>
-                </a>
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+                <div class="container-fluid">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapse_target" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon">
+                        </span>
+                    </button>
+                    <a class="navbar-brand" href="/">
+                        <span class="project-title">
+                            PSE Project
+                        </span>
+                    </a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle end-0" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="bi bi-list icon-dropdown">
+                            </span>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Notifications</a> 
+                        </div>
+                    </li>
+                </div>
             </nav>
         </div>
     )
