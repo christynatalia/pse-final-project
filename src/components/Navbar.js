b2bf26014df62f03a5a79dd7d5d2268e6cb3385f
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-24 17:48:49 +0700

    Dropdown menu is working, merry x'mas eve evryone ^^

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,24 +6,24 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
-                <div class="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapse_target" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon">
                         </span>
                     </button>
-                    <a class="navbar-brand" href="/">
-                        <span class="project-title">
+                    <a className="navbar-brand" href="/">
+                        <span className="project-title">
                             PSE Project
                         </span>
                     </a>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle end-0" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="bi bi-list icon-dropdown">
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle end-0" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span className="bi bi-list icon-dropdown">
                             </span>
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Notifications</a> 
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Notifications</a> 
                         </div>
                     </li>
                 </div>
