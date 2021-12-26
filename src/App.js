285557330b489e499f7a13b1c9c8aee15308abd1
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-26 15:18:53 +0700

    creating the cards for the main page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Navbar from './components/Navbar';
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ItemsGalery from './components/ItemsGalery';
 function App() {
   return (
    <div className="App">
-     <Navbar/>
+     <ItemsGalery/>
    </div>
   );
 }
--- src/components/ItemsGalery.js
+++ src/components/ItemsGalery.js
@@ -1,2 +1,51 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
+
+
+const ItemsGalery = () => {
+    return (
+        <>
+         <Navbar/>
+        <div className="container-fluid">
+            <div className="title-header">
+            <h2 className="text-start header ml-3 p-2">Rooms:</h2>
+            </div>
+            <div className="row">
+                <div className='col-md-4'>
+                    <div class="card ml-3 p-2">
+                        <div class="card-body">
+                            <h5 class="card-title">Room Name</h5>
+                            <p class="card-text">Room Description</p>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-4'>
+                    <div class="card ml-3 p-2">
+                        <div class="card-body">
+                            <h5 class="card-title">Room Name</h5>
+                            <p class="card-text">Room Description</p>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-4'>
+                    <div class="card ml-3 p-2">
+                        <div class="card-body">
+                            <h5 class="card-title">Room Name</h5>
+                            <p class="card-text">Room Description</p>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </>
+        )
+    }
+
+export default ItemsGalery;
