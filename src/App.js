714c9f57cc15a039998547577d336ae273d1f9c1
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-27 12:56:36 +0700

    Trying for the routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Navbar from './components/Navbar';
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ItemsGalery from './components/ItemsGalery';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 function App() {
   return (
-   <div className="App">
-     <ItemsGalery/>
-   </div>
+    <BrowserRouter>
+      <Route path="/" exact component={ItemsGalery}>
+      </Route>
+    </BrowserRouter>
+   
   );
 }
 
