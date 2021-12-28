ab17730b37976ce6f3185d0afe193cce5096d644
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-28 13:50:29 +0700

    added the button for adjusting temperature

--- src/components/SensorsGalery.js
+++ src/components/SensorsGalery.js
@@ -10,16 +10,29 @@ const SensorsGalery = () => {
         <div className="container-fluid">
         <div className="title-header">
             <h2 className="text-start header ml-3 p-2">Sensors:</h2>
-            </div>
+        </div>
+        <div className="container-room-description ml-3 p-2">
+            <p className="text p-0">Room Temperature: x</p>
+            <p className="text p-0">Humidity: x</p>
+        </div>
+
             <div className="row">
                 <div className='col-md-4'>
                     <div className="card ml-3 p-2">
-                        <div className="card-body">
-                            <h5 className="card-title text-center">Sensors Name</h5>
-                            <p className="card-text text-center">Sensors Description</p>
+                        <div className="card-body text-center">
+                            <h5 className="card-title">Sensors Name</h5>
+                            <p className="card-text mx-0 px-0">Sensors Description</p>
+                            <p className="card-text mx-0 px-0">Type: AC</p>
                             <div className="container-button text-center">
                                 <a href="#" className="btn btn-primary turn-button">Turn on</a>
                                 <a href="#" className="btn btn-primary turn-button">Turn off</a>
+                            </div>
+
+                            <div className="container-button mt-3">
+                            <span className="text-temp">Temperature: </span>
+                            <a href="#" className="btn bi bi-dash-circle button-temp"></a>
+                            <a href="#" className="btn bi bi-plus-circle button-temp"></a>
+
                             </div>
                         </div>
                     </div>
