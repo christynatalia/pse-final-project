4e73dd1edb9bfc0257ae33268caa7e25efc4e71d
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-29 13:50:53 +0700

    Fixing the UI for add new sensors

--- src/components/AddSensors.js
+++ src/components/AddSensors.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
+import './AddSensors.css';
 
 
 const AddSensors= () => {
@@ -15,36 +16,47 @@ const AddSensors= () => {
 
             <div className="row">
                 <div className="col-lg-10 col-xl-9 mx-auto">
-                    <div className="card card-style flex-row my-5 bg-light text-white">
+                    <div className="card card-style flex-row my-5 bg-light">
                         <div className="card-body">
                             <h5 className="card-title text-center">New Sensors</h5>
 
-                            <div className="form-label-group">
-                                <input type="text" id="inputTitle"
-                                    className="form-control" placeholder="Give title to your post..." />
-                                <label htmlFor="inputTitle">Name of your sensors</label>
+                            <form>
+                            <div className="form-group row">
+                                <label for="staticName" className="col-sm-2 col-form-label">
+                                    Name
+                                </label>
+                                <div className="col-sm-10">
+                                <input type="text" id="inputTitle" className="form-control" placeholder="Give title to your post..." />
+                                </div>
                             </div>
 
-                            <div className="form-label-group">
-                                <select class="custom-select" id="inputGroupType">
-                                    <option selected>-</option>
-                                    <option value="1">AC</option>
-                                    <option value="2">Lamp</option>
-                                 </select>
+                            <div className="form-group row">
+                                <label for="staticType" className="col-sm-2 col-form-label">
+                                    Type
+                                </label>
+                                <div className = "col-sm-10">
+                                    <select className="form-control" id="inputGroupType">
+                                        <option selected>-</option>
+                                        <option value="1">AC</option>
+                                        <option value="2">Lamp</option>
+                                    </select>
+                                </div>
                             </div>
 
-                            <div className="form-label-group">
-                                <input type="text" id="inputDesc"
-                                    className="form-control" placeholder="Add Type" />
-                                <label htmlFor="inputTag">Description</label>
+                            <div className="form-group row">
+                                <label for="staticDescription" className="col-sm-2 col-form-label">
+                                    Description
+                                </label>
+                                <div className='col-sm-10'>
+                                    <input type="text" id="inputDesc" className="form-control" placeholder="Add Description" />
+                                </div>
                             </div>
-
-            
+                            </form>
 
                             <div className="d-flex justify-content-center">
                                 <div className="d-flex flex-column">
                                     <div className="text-warning"><small></small></div>
-                                    <button className="btn btn-primary">Add Item</button>
+                                    <button className="btn btn-primary m-3">Add Item</button>
                                 </div>
                             </div>
 
